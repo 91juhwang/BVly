be32137862235c78076ab9af644f5de1435e7f39
91juhwang <91juhwang@gmail.com>
2017-09-23 17:51:04 -0400

    Add #update_access_count to increase access_count by 1 when accessed

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,7 +7,7 @@ class UrlsController < ApplicationController
     url_record = Url.find_or_create_by(full_url: full_url)
     @original_url = url_record.full_url
     @encoded_url = url_record.id.to_s(36)
-    @urls = Url.all.order(:access_count).limit(100)
+    @urls = Url.all.order(:access_count).limit(100).reverse
     respond_to { |format| format.js }
   end
 
@@ -16,7 +16,9 @@ class UrlsController < ApplicationController
 
   def redirect
     decoded_url = params[:id].to_i(36)
-    redirect_to Url.find(decoded_url).full_url
+    found_url = Url.find(decoded_url)
+    found_url.update_attributes(access_count: Url.update_access_count(found_url))
+    redirect_to found_url.full_url
   end
 
   private
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,8 @@ class Url < ActiveRecord::Base
   def self.url_to_external(text)
     /\Ahttp(s)?:\/\//.match(text) ? text : text.gsub(/\A(http(s)?:\/\/)?(www\.)?(.*)/,"http\\2://www.\\4")
   end
+
+  def self.update_access_count(found_url)
+    found_url.access_count + 1
+  end
 end
