1f9efc6c75222af807cd6ea25aee27a4f3de5cc6
91juhwang <91juhwang@gmail.com>
2017-09-24 14:17:38 -0400

    Add proper naming to resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   devise_for :users
   resource :users, only: [:show] do
-    resources :user_urls, only: [:index, :create]
+    resources :user_urls, only: [:create]
   end
   resources :urls, only: [:index, :create]
   get '/:id', to: 'urls#redirect', as: 'redirect_original'
