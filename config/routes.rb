2b3154009506670f3acce5a4741461a95358a11c
91juhwang <91juhwang@gmail.com>
2017-09-23 21:48:52 -0400

    Update routes order to invoke root_path first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
   devise_for :users
   resources :urls
-  root to: 'welcome#index'
   get '/:id', to: 'urls#redirect', as: 'redirect_original'
 end
