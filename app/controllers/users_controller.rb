ba5d09f9e11d12dc1c94a3d1dd944b84522a76c5
91juhwang <91juhwang@gmail.com>
2017-09-24 14:17:00 -0400

    Add methods to create associated records in user_urls table to keep track of user specific records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :top_hundred_records, only: [:index, :create]
 
   def show
-    @url = Url.new
-    @urls = recent_hundred_links
-  end
-
-  def create
-    # full_url = Url.url_to_external(url_params['full_url'])
-    # url_record = Url.find_or_create_by(full_url: full_url)
-    # @original_url = url_record.full_url
-    # @encoded_url = url_record.id.to_s(36)
-    # respond_to { |format| format.js }
-  end
-
-  def redirect
-    # decoded_url = params[:id].to_i(36)
-    # found_url = Url.find(decoded_url)
-    # found_url.update_attributes(access_count: Url.update_access_count(found_url))
-    # redirect_to found_url.full_url
+    @user_urls = current_user.urls
+    recent_hundred_links
+    top_hundred_links
   end
 
   private
 
   def recent_hundred_links
-    @urls = UserUrls.all.order(:created_at).limit(100)
+    @recent_urls = current_user.urls.all.order(:created_at).limit(100)
+  end
+
+  def top_hundred_links
+    @urls = current_user.urls.all.order(:access_count).limit(30).reverse
   end
 
   def url_params
