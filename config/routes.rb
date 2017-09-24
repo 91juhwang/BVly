cea6c6dbb19bee5283cc182d176335a06d9128c6
91juhwang <91juhwang@gmail.com>
2017-09-24 01:13:58 -0400

    Add scaffold of user controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   devise_for :users
+  resource :users, only: [:show] do
+    resources :user_urls, only: [:index, :create]
+  end
   resources :urls, only: [:index, :create]
   get '/:id', to: 'urls#redirect', as: 'redirect_original'
 end
